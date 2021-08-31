301e9c31ce8d85481878bc1cbd1370e5d3116dd2
soon <sudosoon@gmail.com>
2021-08-31 18:11:02 +0900

    데이터 저장, 출력 루프

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,9 +1,24 @@
+from django.urls import reverse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
+from accountapp.models import HelloWorld
 
 # Create your views here.
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!'})
+        temp = request.POST.get('hello_world_input')
+
+        # 입력받은 정보 저장
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save();
+
+        # 입력받은 정보 불러옴
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!'})
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
