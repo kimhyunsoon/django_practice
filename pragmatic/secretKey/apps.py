93b5675adb16dd5a4758ede0c3ad4e850a17319a
soon <sudosoon@gmail.com>
2021-08-31 11:07:25 +0900

    github 업로드시 보안 이슈 해결

---
+++ pragmatic/secretKey/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SecretkeyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'secretKey'
---
+++ pragmatic/secretKey/secret_key.py
@@ -0,0 +1,2 @@
+def skey():
+    return 'django-insecure-6h^w9%ppf9dl_)^@p_e=t@dz@lu)oc0z!5&jy0mip74+ebf8)a' 
