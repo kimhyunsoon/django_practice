2d19b9936b44190b4fbbce7db73e8988afa131d7
soon <sudosoon@gmail.com>
2021-08-31 11:35:16 +0900

    예외처리 및 django-environ (보안)

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -11,16 +11,30 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from secretKey.secret_key import skey
+
+import environ,os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = skey
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- pragmatic/secretKey/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SecretkeyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'secretKey'
