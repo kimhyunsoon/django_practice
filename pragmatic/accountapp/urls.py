d4bff9b94f429ca354bc28ef1481dfde12f3cb59
soon <sudosoon@gmail.com>
2021-09-01 15:07:29 +0900

    CreateView 작업

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,8 +1,10 @@
+from accountapp.views import AccountCreateView
 from django.urls import path
 from accountapp.views import hello_world
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,8 +1,16 @@
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from django.views.generic.edit import CreateView
 from accountapp.models import HelloWorld
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# from django.views.generic import CreateView
+
 
 # Create your views here.
 def hello_world(request):
@@ -14,6 +22,7 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save();
 
+        print(new_hello_world.text)
         # 입력받은 정보 불러옴
         hello_world_list = HelloWorld.objects.all()
 
@@ -22,3 +31,10 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
 
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
